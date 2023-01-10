79aa40238fe43881260309e8b7ae4f3bd56be5d8
ZorroSavage117 <zorrosavage117@byui.edu>
2023-01-10 14:58:05 -0700

    in class

---
+++ Class/name.py
@@ -0,0 +1,19 @@
+# page start
+print()
+
+# varables
+name = ""
+year_born = 0
+this_year = 2023
+age = 0
+
+# main code
+name = input("What is you name? ")
+year_born = int(input("What year were you born? "))
+
+age = this_year - year_born
+
+print(f"You are {name} and you are {age} years old.")
+
+# page end
+print()
